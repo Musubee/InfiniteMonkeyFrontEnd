ccf34e28b6bbf8ae578410651c60b580ec80bd98
Josh Kuroda <jkkealii@yahoo.com>
2022-02-09 17:19:22 -0800

    removing App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { Box, Grommet, Header, Heading, Main } from "grommet";
 import { Gremlin } from "grommet-icons";
-import "./App.css";
 
 import About from "./components/About";
 
